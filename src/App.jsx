ca33392d53f75bf5b6175119dd78fa9aa63dfe71
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-15 16:12:35 +0100

    Set state for steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
@@ -5,7 +7,16 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    step > 1 && setStep(step - 1);
+  }
+
+  function handleNext() {
+      step < 3 && setStep(step + 1);
+  }
+
   return (
   <>
       <div className="steps">
@@ -16,8 +27,16 @@ function App() {
           </div>
           <p className="message">Step {step}: {messages[step - 1]}</p>
           <div className="buttons">
-              <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-              <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+              <button
+                  style={{backgroundColor: '#7950f2', color: '#fff'}}
+                  onClick={handlePrevious}
+              > Previous
+              </button>
+              <button
+                  style={{backgroundColor: '#7950f2', color: '#fff'}}
+                  onClick={handleNext}
+              >Next
+              </button>
           </div>
       </div>
   </>
