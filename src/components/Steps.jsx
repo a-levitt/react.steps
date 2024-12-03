998436b96461df3e925a2dca8a5b9166fdfca28c
Artem Trifonov <artemis.levitt@gmail.com>
2024-12-03 15:42:13 +0100

    Separate step-message component with child props

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 
 import Button from './Button.jsx'
+import StepMessage from './StepMessage.jsx'
 
 const messages = [
     "Learn React ⚛️",
@@ -34,7 +35,9 @@ function Steps() {
                       <div className={step >= 2 ? 'active' : ''}>2</div>
                       <div className={step >= 3 ? 'active' : ''}>3</div>
                   </div>
-                  <p className="message">Step {step}: {messages[step - 1]}</p>
+                  <StepMessage step={step}>
+                      {messages[step - 1]}
+                  </StepMessage>
                   <div className="buttons">
                      <Button
                          textColor='#fff'
